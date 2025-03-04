77cf6af72eac1ff73b6ce532c0bd89c3d6b1b98a
Sergey Krash <badj.xy@gmail.com>
2025-03-05 00:14:36 +0400

    lesson: Сторонние библиотеки

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,10 @@
 from brain_games.module import greet
+from brain_games.cli import welcome_user
 
 
 def main():
-    greet()
+    # greet()
+    welcome_user()
 
 if __name__ == "__main__":
     main()
